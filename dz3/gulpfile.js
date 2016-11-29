ea79a0582d0c3c8be1a53a3aafee432bae76e6f0
sergostapchuk888 <sergostapchuk888@gmail.com>
2016-11-29 16:34:56 +0200

    realase fitness 1.2

--- dz3/gulpfile.js
+++ dz3/gulpfile.js
@@ -3,7 +3,9 @@ var gulp         = require('gulp'),
    sass         = require('gulp-sass'),
    autoprefixer = require('autoprefixer'),
    browser      = require('browser-sync'),
-   sourcemaps   = require('gulp-sourcemaps');
+   sourcemaps   = require('gulp-sourcemaps'),
+   iconfont = require("gulp-iconfont"),
+   consolidate = require("gulp-consolidate");
 
 
 gulp.task('sass', function () {
@@ -28,3 +30,26 @@ gulp.task('default',['serve'], function() {
  gulp.watch(['sass/**/*.scss'], ['sass']); 
  gulp.watch('./**/*.html').on('change', browser.reload); 
 });
+gulp.task("build:icons", function() {
+   return gulp.src(["./icons/*.svg"]) //path to svg icons
+     .pipe(iconfont({
+       fontName: "myicons",
+       formats: ["ttf", "eot", "woff", "svg"],
+       centerHorizontally: true,
+       fixedWidth: true,
+       normalize: true
+
+     }))
+     .on("glyphs", (glyphs) => {
+
+       gulp.src("./icons/util/*.scss") // Template for scss files
+           .pipe(consolidate("lodash", {
+               glyphs: glyphs,
+               fontName: "myicons",
+               fontPath: "../../../fonts/"
+           }))
+           .pipe(gulp.dest("./sass/icons/")); // generated scss files with classes
+     })
+     .pipe(gulp.dest("./fonts/")); // icon font destination
+});
+ 
